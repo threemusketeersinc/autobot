23879af7768f9b2547d8ab4825270c22e33d8dba
Three Musketeers <threemusketeers.tg@gmail.com>
2020-02-05 16:24:51 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+print('welcome to my bot  \n @virtualjust0and1') 
+
+print('choose mode')
+print('1. scrapper')
+print('2. adder')
+
+mode = str(input('enter number: '))
+if mode == str(1):
+    import main
+
+elif mode == str(2):
+    import extra
+
+
+"""this bot made by @virtualjust0and1"""
